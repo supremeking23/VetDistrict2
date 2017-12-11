7c27224f043802038b42d7f420fe842c05a2cf59
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2017-12-11 15:11:28 +0800

    second commit adds basic pages landing page logins in adminn and customer,  no view yet in employee

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -3,8 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+
+
+
 	public function dashboard(){
 		$this->load->view('admin/dashboard');
+	}
+
+
+	public function login(){
+		$this->load->view('admin/login');
 	}
 
 
